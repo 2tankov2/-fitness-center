7a362a2b5149a8d7e0f74bedb372a7467a43fce9
tankov <tan.kov@hotmail.com>
2020-06-12 21:21:36 +0300

    добавляет слайдер на блок <тренеры>

--- source/js/main.js
+++ source/js/main.js
@@ -10,4 +10,34 @@
   };
 
   window.IMask(orderPhone, maskOptions);
+
 }());
+
+window.onload = function () {
+  var trainersSwiper = new Swiper ('.trainers-swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 40,
+    slidesPerGroup: 4,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var reviewsSwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+};
