6a626119d01a3078f80d507d8a247a40ab8f6d8d
tankov <tan.kov@hotmail.com>
2020-06-13 09:09:57 +0300

    Update 2020-06-13T06:09:54.990Z

--- js/main.js
+++ js/main.js
@@ -10,4 +10,51 @@
   };
 
   window.IMask(orderPhone, maskOptions);
+
 }());
+
+window.onload = function () {
+  var trainersSwiper = new Swiper ('.trainers-swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: getPerViews(),
+    /*slidesPerView: 4,*/
+    slidesPerGroup: 4,
+    spaceBetween: 40,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.button--trainers-next',
+      prevEl: '.button--trainers-back',
+    },
+    on: {
+      resize: function () {
+        trainersSwiper.update(getPerViews());
+      }
+    }
+  });
+
+  function getPerViews() {
+    var windowWidth = window.innerWidth;
+    var slidesPerView = windowWidth <= 768 ? 2 : 4;
+
+    return slidesPerView;
+  }
+
+  var reviewsSwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    effect: 'flip',
+    grabCursor: true,
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.button--reviews-next',
+      prevEl: '.button--reviews-back',
+    },
+  });
+};
